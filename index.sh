c21ac67d8aaf012225c84866e9f35142fc98a5f4
afeiship <1290657123@qq.com>
2020-05-18 07:55:34 +0800

    fix: deno home

--- index.sh
+++ index.sh
@@ -3,7 +3,7 @@
 ROOT_PATH=$(dirname $BASH_SOURCE);
 
 # alias xxx='cd -';
-export DENO_INSTALL="$HEME/.deno"
+export DENO_INSTALL="$HOME/.deno"
 export PATH="$DENO_INSTALL/bin:$PATH"
 
 unset ROOT_PATH;
